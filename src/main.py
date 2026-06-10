4cd5a2383aab83c09f295b7cded648c1892d469b
Vitali Agashkov <vitali.agashkov@gmail.com>
2026-06-10 16:06:44 +0300

    Initial pull request

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/")
+def process_message(payload: dict):
+    answer = dict()
+    answer["code"] = 0
+    answer["uid"] = payload.get("uid")
+    answer["amount"] = payload.get("amount")
+    print(answer)
